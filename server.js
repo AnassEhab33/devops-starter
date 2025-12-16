75bb65103056bc5d28f84763d67ebf2925354bd9
AnassEhab <anassehab33@gmail.com>
2025-12-16 13:22:27 +0200

    Modified the Port variable to be Dynamically Assigned by the cloud to easy deploy it in based on the enviornment variable that they provide

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Define a route that returns JSON
 app.get('/', (req, res) => {
