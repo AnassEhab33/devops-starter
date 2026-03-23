3e57671ed46ec19248feafdcd9c3df4fd9b3df12
Anass_Ehab33 <anassehab33@gmail.com>
2026-03-23 16:19:23 +0200

    make the server run with nodemon for development purposes

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.get('/', (req, res) => {
     // .json() automatically sets the Content-Type header to application/json
     res.json({
         status: 'success',
-        message: 'Hello! This is a Anass Einshouka response.',
+        message: 'Hello! This is a Anass Einshouka Elgamed response.',
         timestamp: new Date()
     });
 });
